f3b2e3a078381d25d4de6abb9f00166c217ecb91
Zaid <71690459+Zaid97-kai@users.noreply.github.com>
2024-02-03 11:30:42 +0300

    Add http client service

---
+++ TaskNinjaHub.Desktop/Models/UserTokenModel.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace TaskNinjaHub.Desktop.Models;
+
+public class UserTokenModel
+{
+    [JsonProperty("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")]
+    public string UserName { get; set; }
+
+    [JsonProperty("first_name")]
+    public string FirstName { get; set; }
+
+    [JsonProperty("last_name")]
+    public string LastName { get; set; }
+
+    [JsonProperty("middle_name")]
+    public string MiddleName { get; set; }
+
+    [JsonProperty("short_name")]
+    public string ShortName { get; set; }
+
+    [JsonProperty("email")]
+    public string Email { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    public string Token { get; set; }
+
+    public List<string> Claims { get; set; }
+}
--- TaskNinjaHub.Desktop/Services/UserProvider.cs
+++ TaskNinjaHub.Desktop/Services/UserProvider.cs
@@ -1,8 +1,51 @@
-﻿using System.Security.Claims;
+﻿using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using TaskNinjaHub.Desktop.Models;
 
 namespace TaskNinjaHub.Desktop.Services;
 
 public class UserProvider
 {
-    public static IEnumerable<Claim> User { get; set; }
+    public static UserTokenModel User { get; set; } = null!;
+
+    public static UserTokenModel GetUser(string token)
+    {
+        var jwtSecurityToken = new JwtSecurityToken(jwtEncodedString: token);
+        var userToken = new UserTokenModel
+        {
+            Token = token
+        };
+
+        foreach (var claim in jwtSecurityToken.Claims)
+        {
+            switch (claim.Type)
+            {
+                case "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name":
+                    userToken.Name = claim.Value;
+                    break;
+                case "first_name":
+                    userToken.FirstName = claim.Value;
+                    break;
+                case "last_name":
+                    userToken.LastName = claim.Value;
+                    break;
+                case "middle_name":
+                    userToken.MiddleName = claim.Value;
+                    break;
+                case "short_name":
+                    userToken.ShortName = claim.Value;
+                    break;
+                case "email":
+                    userToken.Email = claim.Value;
+                    break;
+                case "claims":
+                    userToken.Claims = JsonConvert.DeserializeObject<List<string>>(claim.Value) ?? new List<string>(); ;
+                    break;
+            }
+        }
+        
+        User = userToken;
+        
+        return userToken;
+    }
 }
