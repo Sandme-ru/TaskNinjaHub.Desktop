1f076b79e6a83bff6df42557f5bffb9fbf9826b4
Shvyrkaloff <81988933+Shvyrkaloff@users.noreply.github.com>
2024-05-12 15:55:14 +0300

    add create user

--- TaskNinjaHub.Desktop/Windows/Users/Create/CreateUserWindow.xaml.cs
+++ TaskNinjaHub.Desktop/Windows/Users/Create/CreateUserWindow.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using System.Windows;
 using TaskNinjaHub.Desktop.Models.User;
-using TaskNinjaHub.Desktop.Services.HttpClientServices;
+using TaskNinjaHub.Desktop.Services.UserServices.Roles;
 using TaskNinjaHub.Desktop.Services.UserServices.Users;
 using TaskNinjaHub.Desktop.Utils.HttpClientFactory;
 using TaskNinjaHub.Desktop.Utils.Storages;
-using TaskNinjaHub.Desktop.Windows.InformationSystems.List;
+using TaskNinjaHub.Desktop.Windows.Users.List;
 
 namespace TaskNinjaHub.Desktop.Windows.Users.Create;
 
@@ -15,15 +15,31 @@ public partial class CreateUserWindow : Window
 {
 
     private UserService _userService;
+
+    private RoleService _roleService;
+
     public CreateUserWindow()
     {
         InitializeComponent();
         NameTextBlock.Text = PropertyStorage.Username;
     }
+    public async Task PopulateRoleComboBox()
+    {
+        var roles = await GetAllTypes();
+        foreach (var role in roles)
+        {
+            RoleComboBox.Items.Add(role.Name);
+        }
+    }
+    private async Task<IEnumerable<Role>> GetAllTypes()
+    {
+        return await _roleService.GetAllAsync();
+    }
 
-    public void InjectTaskTypeService(UserService userService)
+    public void InjectTaskTypeService(UserService userService, RoleService roleService)
     {
         _userService = userService;
+        _roleService = roleService;
     }
 
     private async void CreateButton_Click(object sender, RoutedEventArgs e)
@@ -32,11 +48,14 @@ public partial class CreateUserWindow : Window
         {
             UserDto userDto = new UserDto()
             {
-                Email = NameBox.Text,
+                Email = EmailBox.Text,
+                UserName = EmailBox.Text,
                 FirstName = NameBox.Text,
-                LastName = NameBox.Text,
-                MiddleName = NameBox.Text,
-                PhoneNumber = NameBox.Text,
+                LastName = SurnameBox.Text,
+                MiddleName = MiddleBox.Text,
+                PhoneNumber = PhoneBox.Text,
+                Password = PasswordBox.Text,
+                Role = RoleComboBox.SelectedItem.ToString(),
             };
 
             var result = await _userService.AddUserAsync(userDto);
@@ -44,11 +63,11 @@ public partial class CreateUserWindow : Window
             if (result.IsSuccessStatusCode)
             {
                 MessageBox.Show("Пользователь успешно добавлен");
-                InformationSystemService informationSystemService = new InformationSystemService(new HttpClientFactory());
-                InformationSystemListWindow window = new InformationSystemListWindow();
-                window.InjectTaskTypeService(informationSystemService);
-                window.Show();
+                UserListWindow roleListWindow = new();
+                UserService userService = new UserService(new HttpClientFactory());
+                roleListWindow.InjectTaskTypeService(userService);
                 this.Hide();
+                roleListWindow.Show();
             }
         }
         else
@@ -59,19 +78,24 @@ public partial class CreateUserWindow : Window
 
     private void backButton_Click(object sender, RoutedEventArgs e)
     {
-        InformationSystemService informationSystemService = new InformationSystemService(new HttpClientFactory());
-        InformationSystemListWindow window = new InformationSystemListWindow();
-        window.InjectTaskTypeService(informationSystemService);
-        window.Show();
+        UserListWindow roleListWindow = new();
+        UserService userService = new UserService(new HttpClientFactory());
+        roleListWindow.InjectTaskTypeService(userService);
         this.Hide();
+        roleListWindow.Show();
     }
 
     private void UpdateButton_Click(object sender, RoutedEventArgs e)
     {
-        InformationSystemService informationSystemService = new InformationSystemService(new HttpClientFactory());
-        InformationSystemListWindow window = new InformationSystemListWindow();
-        window.InjectTaskTypeService(informationSystemService);
-        window.Show();
+        UserListWindow roleListWindow = new();
+        UserService userService = new UserService(new HttpClientFactory());
+        roleListWindow.InjectTaskTypeService(userService);
         this.Hide();
+        roleListWindow.Show();
+    }
+
+    private async void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        await PopulateRoleComboBox();
     }
 }
--- TaskNinjaHub.Desktop/Windows/Users/List/UserListWindow.xaml.cs
+++ TaskNinjaHub.Desktop/Windows/Users/List/UserListWindow.xaml.cs
@@ -2,12 +2,14 @@
 using System.Windows.Controls;
 using TaskNinjaHub.Desktop.Models.User;
 using TaskNinjaHub.Desktop.Services.HttpClientServices;
+using TaskNinjaHub.Desktop.Services.UserServices.Roles;
 using TaskNinjaHub.Desktop.Services.UserServices.Users;
 using TaskNinjaHub.Desktop.Utils.HttpClientFactory;
 using TaskNinjaHub.Desktop.Utils.Storages;
 using TaskNinjaHub.Desktop.Windows.InformationSystems.Create;
 using TaskNinjaHub.Desktop.Windows.InformationSystems.Update;
 using TaskNinjaHub.Desktop.Windows.Menu;
+using TaskNinjaHub.Desktop.Windows.Users.Create;
 
 namespace TaskNinjaHub.Desktop.Windows.Users.List
 {
@@ -71,11 +73,12 @@ namespace TaskNinjaHub.Desktop.Windows.Users.List
 
         private void CreateButton_Click(object sender, RoutedEventArgs e)
         {
-            CreateInformationSystemWindow createInformationSystemWindow = new CreateInformationSystemWindow();
-            InformationSystemService informationSystemService = new InformationSystemService(new HttpClientFactory());
-            createInformationSystemWindow.InjectTaskTypeService(informationSystemService);
+            CreateUserWindow createUserWindow = new();
+            UserService userService = new UserService(new HttpClientFactory());
+            RoleService roleService = new RoleService(new HttpClientFactory());
+            createUserWindow.InjectTaskTypeService(userService, roleService);
             this.Hide();
-            createInformationSystemWindow.Show();
+            createUserWindow.Show();
         }
 
         private void backButton_Click(object sender, RoutedEventArgs e)
